4c1d28e61c0698f4d6a73e2fec460cc4edcb9f97
Bas <bastiansg.bas@gmail.com>
2025-07-16 23:19:06 -0300

    add TokenSplitter

---
+++ rage/splitters/token_splitter.py
@@ -0,0 +1,34 @@
+from more_itertools import flatten
+
+from langchain_text_splitters import TokenTextSplitter
+from rage.meta.interfaces import TextSplitter, Document, TextChunk
+
+
+class TokenSplitter(TextSplitter):
+    def __init__(
+        self,
+        chunk_size: int = 256,
+        chunk_overlap: int = 16,
+    ):
+        super().__init__()
+
+        self.spliter = TokenTextSplitter(
+            model_name="gpt-4o",
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+        )
+
+    def get_text_chunks(self, document: Document) -> TextChunk:
+        text_chunks = self.spliter.split_text(text=document.text)
+        return [
+            TextChunk(
+                text=text,
+                metadata=document.metadata,
+                num_tokens=self._get_num_tokens(text=text),
+            )
+            for text in text_chunks
+        ]
+
+    def _split_documents(self, documents=list[Document]) -> list[TextChunk]:
+        text_chunks = map(self.get_text_chunks, documents)
+        return list(flatten(text_chunks))
